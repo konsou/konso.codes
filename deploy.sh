3c0168f95e425670b3e0bc539981e475b408215b
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-03-15 17:15:09 +0200

    Typo fix in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ source .env
 ./compile.sh
 echo "Deploying..."
 scp -r -P $SSH_PORT html/ "${SSH_USER}@${SSH_SERVER}:${REMOTE_TEMP_DIR}/"
-ssh "${SSH_USER}@${SSH_SERVER}" -p $SSH_PORT -t "sudo cp -r \"${REMOTE_TEMP_DIR}/html/*\" \"${REMOTE_WWW_DIR}/\""
+ssh "${SSH_USER}@${SSH_SERVER}" -p $SSH_PORT -t "sudo cp -r \"${REMOTE_TEMP_DIR}/html/\" \"${REMOTE_WWW_DIR}/\""
