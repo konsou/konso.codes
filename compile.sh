0a2b5f7d74d7d6f332254105ddd6c7261d9dfd5d
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-03-15 14:11:56 +0200

    Add images to compile process

--- compile.sh
+++ compile.sh
@@ -3,5 +3,14 @@ if [[ ! -d ./html ]]; then
     echo "Creating html directory..."
     mkdir ./html
 fi
+
+if [[ ! -d ./html/images ]]; then
+    echo "Creating html images directory..."
+    mkdir ./html/images
+fi
+
 echo "Compiling HTML..."
 pandoc index.md -f markdown -t html -s -o html/index.html
+
+echo "Copying images..."
+cp images/* html/images/
