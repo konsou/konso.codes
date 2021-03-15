51d735ccb319d8d8b89df21a0018eae2728b5111
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-03-15 13:47:10 +0200

    Delete package.json. Update compile.sh to create html dir if needed.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if [[ ! -d ./html ]]; then
+    echo "Creating html directory..."
+    mkdir ./html
+fi
 echo "Compiling HTML..."
 pandoc index.md -f markdown -t html -s -o html/index.html
