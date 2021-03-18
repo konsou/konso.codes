0d99465b9f3c694eff539877fcb70f86be35c681
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-03-18 14:11:39 +0200

    Hopefully fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ source .env
 echo "Deploying..."
 scp -r -P $SSH_PORT html/ "${SSH_USER}@${SSH_SERVER}:${REMOTE_TEMP_DIR}/"
 # TODO: FIX THIS COMMAND
-ssh "${SSH_USER}@${SSH_SERVER}" -p $SSH_PORT -t "sudo cp -r \"${REMOTE_TEMP_DIR}/html/\" \"${REMOTE_WWW_DIR}/\""
+ssh "${SSH_USER}@${SSH_SERVER}" -p $SSH_PORT -t "sudo cp -r \"${REMOTE_TEMP_DIR}/.\" \"${REMOTE_WWW_DIR}/html/\""
