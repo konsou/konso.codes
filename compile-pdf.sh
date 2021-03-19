57ed883302ccdaff27f7da28c79327f11d450551
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-03-19 13:35:08 +0200

    Can now generate pdf

---
+++ compile-pdf.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [[ ! -d ./pdf ]]; then
+    echo "Creating pdf directory..."
+    mkdir ./pdf
+fi
+
+echo "Compiling PDF..."
+pandoc index.md \
+    -f markdown \
+    -t pdf \
+    -s \
+    -o pdf/CV-Tomi-Javanainen.pdf \
+    --metadata-file metadata.json \
+    -V linkcolor:blue \
+    -V geometry:a4paper \
+    -V mainfont="Georgia Serif" \
+    -V monofont="Menlo Monaco 'Lucida Console' Consolas monospace" \
+    -V fontsize=14pt \
