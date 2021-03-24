ac75f13a74244bde31101c4fc48285869416d1dc
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-03-24 14:31:57 +0200

    Try getting deploy.sh to work

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+if [[ ! -f .env ]]; then
+    echo ".env missing!"
+    exit 1
+fi
 source .env
-./compile.sh
+./compile-pdf.sh
+git add .
+git commit -m "Automatic deployment"
+git push
 echo "Deploying..."
-scp -r -P $SSH_PORT html/ "${SSH_USER}@${SSH_SERVER}:${REMOTE_TEMP_DIR}/"
-# TODO: FIX THIS COMMAND
-ssh "${SSH_USER}@${SSH_SERVER}" -p $SSH_PORT -t "sudo cp -r \"${REMOTE_TEMP_DIR}/.\" \"${REMOTE_WWW_DIR}/html/\""
+DEPLOY_ON_SERVER_SCRIPT=$(< deploy-on-server.sh)
+TEMP_SCRIPT_LOCATION=/tmp/konso.codes/temp-deploy-script.sh
+ssh "${SSH_USER}@${SSH_SERVER}" -p $SSH_PORT -t "echo "${DEPLOY_ON_SERVER_SCRIPT}" > "${TEMP_SCRIPT_LOCATION}"; bash "${TEMP_SCRIPT_LOCATION}"; rm "${TEMP_SCRIPT_LOCATION}""
