5365b3c3d23c2e34b9a0c8d8f17a55d9e6ff131e
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-03-24 14:38:53 +0200

    Automatic deployment

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ git push
 echo "Deploying..."
 DEPLOY_ON_SERVER_SCRIPT=$(< deploy-on-server.sh)
 TEMP_SCRIPT_LOCATION=/tmp/konso.codes/temp-deploy-script.sh
-ssh "${SSH_USER}@${SSH_SERVER}" -p $SSH_PORT -t "echo "${DEPLOY_ON_SERVER_SCRIPT}" > "${TEMP_SCRIPT_LOCATION}"; bash "${TEMP_SCRIPT_LOCATION}"; rm "${TEMP_SCRIPT_LOCATION}""
+ssh "${SSH_USER}@${SSH_SERVER}" -p $SSH_PORT -t "echo \"${DEPLOY_ON_SERVER_SCRIPT}\" > \"${TEMP_SCRIPT_LOCATION}\"; bash \"${TEMP_SCRIPT_LOCATION}\"; rm \"${TEMP_SCRIPT_LOCATION}\""
